435714be67d0b63ebe26dc5ecfb8b93f74b8cc40
Zetalol <gerorugh@gmail.com>
2025-04-27 18:48:41 +0200

    phpdoc connexio

--- Connexio.php
+++ Connexio.php
@@ -1,21 +1,46 @@
 <?php
+/**
+ * Gestor de connexions MySQL mitjançant mysqli.
+ *
+ * @author  Carles Canals Gozálvez
+ * @version 1.0
+ */
 
-class Connexio {
-    //Dades de la connexió a la base de dades la_meva_botiga.
-    private $host = "localhost";
-    private $usuario = "root";
-    private $contraseña = "";
-    private $baseDatos = "la_meva_botiga";
+class Connexio
+{
+    /** @var string $servidor Nom del servidor MySQL */
+    private string $servidor = "localhost";
 
-    public function obtenirConnexio() {
-        $conexion = new mysqli($this->host, $this->usuario, $this->contraseña, $this->baseDatos);
+    /** @var string $usuari Usuari de la BD */
+    private string $usuari   = "root";
+
+    /** @var string $contrasenya Contrasenya de la BD */
+    private string $contrasenya = "root";
+
+    /** @var string $baseDades Nom de la base de dades */
+    private string $baseDades = "la_meva_botiga";
+
+    /**
+     * Obté un objecte mysqli amb la connexió oberta.
+     *
+     * @throws \RuntimeException Si la connexió falla.
+     * @return \mysqli Connexió a la base de dades.
+     */
+    public function obtenirConnexio(): mysqli
+    {
+        $conexion = new mysqli(
+            $this->servidor,
+            $this->usuari,
+            $this->contrasenya,
+            $this->baseDades
+        );
 
         if ($conexion->connect_error) {
-            die("Error de conexión: " . $conexion->connect_error);
+            throw new \RuntimeException(
+                "Error de connexió: " . $conexion->connect_error
+            );
         }
-
         return $conexion;
     }
 }
-
 ?>
