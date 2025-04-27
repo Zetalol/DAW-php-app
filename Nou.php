e5c217f0a47144425340fccfa3fcf79c6b319a8f
Zetalol <gerorugh@gmail.com>
2025-04-27 17:17:58 +0200

    Afegit missatge

--- Nou.php
+++ Nou.php
@@ -1,7 +1,7 @@
 <?php
 require_once('Connexio.php');
 require_once('Header.php');
-
+//Missatge conflictiu
 // Crear connexió
 $connexioObj = new Connexio();
 $connexio = $connexioObj->obtenirConnexio();
